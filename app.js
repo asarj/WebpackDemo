242286154586da7c4e36e68e30208643cb686d30
AjaySarjooCSE219 <ajay.sarjoo@stonybrook.edu>
2019-01-17 22:10:04 -0500

    Figured out how to bundle files in webpack

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 var name = require("./name")
 console.log("Hello: " + name.name);
+console.log("Hello World")
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  entry: './app.js',
+  output: {
+    filename: 'build.js',
+    path: __dirname
+  },
+  watch: true
+};
