73a269a4956cb3cc080292353fbeddd64dce8097
AjaySarjooCSE219 <ajay.sarjoo@stonybrook.edu>
2019-01-20 14:21:27 -0500

    Implemented changes with the css extract plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
+const ExtractTextPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: './app.js',
   output: {
     filename: './build.js',
     path: __dirname
   },
+  plugins:[
+    new ExtractTextPlugin("dist/style.scss")
+  ],
   watch: true,
   resolveLoader: {
     moduleExtensions: ["-loader"]
@@ -11,20 +15,20 @@ module.exports = {
   /*
     IMPROVEMENT: I used the webpack docs to integrate the same changes in EP3
     to the newer changes reflected in the docs, the video tutorial follows the
-    old format
+    old webpack v2
   */
   module: {
         rules: [
-            {
-              test: /\.js$/,
-              enforce: "pre",
-              exclude: /node_modules/,
-              use:[
-                {
-                  loader: `jshint-loader`,
-                }
-              ]
-            },
+            // {
+            //   test: /\.js$/,
+            //   enforce: "pre",
+            //   exclude: /node_modules/,
+            //   use:[
+            //     {
+            //       loader: ["jshint-loader"],
+            //     }
+            //   ]
+            // },
             {
                 test: /.js$/,
                 loader: "babel-loader",
@@ -41,10 +45,18 @@ module.exports = {
                 test: /\.scss$/,
                 loader: ['style-loader', 'css-loader?url=false', 'sass-loader'],
             },
+            // IMPROVEMENT: used updated docs to implement extract text with the mini-css-extract-plugin
+            {
+                test: /\.scss@/,
+                use: [
+                    ExtractTextPlugin.loader, "css-loader", "sass-loader",
+                ]
+            },
             {
                 test: /\.css$/,
                 loader: ['style-loader', 'css-loader?url=false']
             }
         ]
     },
+
 };
