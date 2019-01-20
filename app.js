7edae84d73c1020fc205822d85e970b913ff884c
AjaySarjooCSE219 <ajay.sarjoo@stonybrook.edu>
2019-01-20 13:28:07 -0500

    Fixed issues with css loader and sass loader

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("./style.scss");
 var name = require("./name")
 console.log("Hello: " + name.name);
 console.log("Hello World")
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,31 @@
 module.exports = {
   entry: './app.js',
   output: {
-    filename: 'build.js',
+    filename: './build.js',
     path: __dirname
   },
-  watch: true
+  watch: true,
+  resolveLoader: {
+    moduleExtensions: ["-loader"]
+  },
+  module: {
+        rules: [
+            {
+                test: /.js$/,
+                exclude: /node_modules/,
+            },
+            {
+                test: /.less$/,
+                loader: ['style-loader', 'css-loader?url=false', 'less-loader'],
+            },
+            {
+                test: /\.scss$/,
+                loader: ['style-loader', 'css-loader?url=false', 'sass-loader'],
+            },
+            {
+                test: /\.css$/,
+                loader: ['style-loader', 'css-loader?url=false']
+            }
+        ]
+    },
 };
